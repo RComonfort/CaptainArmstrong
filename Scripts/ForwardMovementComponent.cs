b6c4a99b599c60396eabdfe944d0f773cb823873
R. Comonfort <a.comonfort@outlook.com>
2019-12-09 22:46:00 -0600

    beginning project. Working on forward movement component

---
+++ Scripts/ForwardMovementComponent.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class ForwardMovementComponent : MonoBehaviour
+{
+	[SerializeField] private float acceleration = 10f;
+    [SerializeField] private float turnSpeed = 360; 
+	
+	private Rigidbody2D rb;
+	private float anglesToRotate = 0f;
+
+	private void Awake() {
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void AddRotationAngles(float angles)
+	{
+		anglesToRotate = angles;
+	}
+
+	private void FixedUpdate() {
+		
+		//Rotate towards dir
+		if (!Mathf.Approximately(anglesToRotate, 0f))
+		{
+			float angleDelta = turnSpeed * Time.fixedDeltaTime;
+
+			anglesToRotate = Mathf.Sign(anglesToRotate) * Mathf.Abs(anglesToRotate) - angleDelta;
+
+			Quaternion targetRot = Quaternion.AngleAxis(anglesToRotate, Vector3.forward);
+        	Quaternion newRot = Quaternion.RotateTowards(targetRot, transform.rotation, angleDelta);
+
+        	rb.MoveRotation(newRot);
+		}
+			
+        //Move forwards
+        rb.AddForce(transform.forward * acceleration * Time.fixedDeltaTime);
+	}
+
+	private void OnTriggerEnter2D(Collider2D other) {
+		
+	}
+}
