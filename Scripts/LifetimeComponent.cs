909a0f50e2e787a8fa3097a7b2519e303f78fa2d
R. Comonfort <a.comonfort@outlook.com>
2019-12-11 12:19:26 -0600

    Lifetime component used to kill objects automatically

--- Scripts/LifetimeComponent.cs
+++ Scripts/LifetimeComponent.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class LifetimeComponent : MonoBehaviour
 {
+	[SerializeField] private EDestroyPolicy destroyBy;
+
+	//Time before destruction. If destroy policy is time, lifetime is considered from Start(). If it is by leaving screen, it will count after object stops rendering the first time
+	[SerializeField] private float lifetime = 100f;    
+	
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (destroyBy == EDestroyPolicy.ByTime)
+			Invoke("DestroyObj", lifetime);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private void OnBecameInvisible() 
+	{
+		if (destroyBy == EDestroyPolicy.ByLeavingScreen)
+			Invoke("DestroyObj", lifetime);
+	}
+
+	private void OnBecameVisible() {
+		if (destroyBy == EDestroyPolicy.ByLeavingScreen)
+			CancelInvoke();
+	}
+
+	private void DestroyObj()
+	{
+		Destroy(this);
+	}
 }
